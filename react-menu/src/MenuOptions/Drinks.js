5063493d716acbb704bda3d732ae0f410f6901a6
Philippe Recto <precto1285@gmal.com>
2019-03-08 10:45:50 -0800

    set drinks menu

--- react-menu/src/MenuOptions/Drinks.js
+++ react-menu/src/MenuOptions/Drinks.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card } from 'reactstrap';
 
 const Drinks = props => {
+
+  const [drnkz, setDrnkz] = useState({
+    showDrinks: false
+  }),
+
+    clickHandler = () => {
+      const doesShow = drnkz.showDrinks;
+      setDrnkz({
+        drinks: ['Calamansi Juice', 'Buko Juice', 'Sago\'t Gulaman'],
+        showDrinks: !doesShow
+      })
+    }
+
   return (
-    <div className='card my-5'>
-      <h1>{props.title}</h1>
-    </div>
+    <Card>
+      <div className="card-header"><h1>{props.title}</h1></div>
+      <div className="card-body text-left">
+        {
+          drnkz.showDrinks ?
+            <ul>
+              <li>{drnkz.drinks[0]}</li>
+              <li>{drnkz.drinks[1]}</li>
+              <li>{drnkz.drinks[2]}</li>
+            </ul>
+            : null
+        }
+      </div>
+      <div className="card-footer">
+        <button onClick={clickHandler} className="btn btn-primary">Show</button>
+      </div>
+    </Card>
   )
 }
 
